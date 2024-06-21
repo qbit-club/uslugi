4abfaed8ee1e00b25aa9b12abfb69c19effa9fa4
JetGreen <grishadzyin@gmail.com>
2024-06-21 18:28:45 +0400

    add create hall router

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,14 @@
 useHead({
   title: 'Рестик'
 })
+
+const router = useRouter()
 </script>
 
 <template>
   <v-row>
     <v-col :cols="12">
-
+      <v-btn block @click="router.push('/hall/create')" class="ma-6 bg-blue h-200">создать зал</v-btn>
     </v-col>
   </v-row>
 </template>
