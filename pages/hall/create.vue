f065fd4a4f0803746cf4d2fb759815cb9fd284f8
JetGreen <grishadzyin@gmail.com>
2024-06-22 16:45:39 +0400

    display tables

--- pages/hall/create.vue
+++ pages/hall/create.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
-import { useField, useForm } from 'vee-validate'
-import { routerKey } from 'vue-router';
+// meta
+useHead({
+  title: 'Рестик | Создать зал'
+})
 
-let loading = ref(false)
+// all types
+import type { Table } from '../../types/table.interface'
+
+// other imports
+import { useField, useForm } from 'vee-validate'
+
 const hallStore = useHall()
 const router = useRouter()
+
+let loading = ref(false)
+let tablePage = ref(0)
+
+const tablesHeaders = [
+  { title: 'Номер места', key: 'number' },
+  { title: 'Этаж', key: 'floor' },
+  { title: 'Кол-во мест', key: 'seatsNumber' },
+]
 
 const { meta, handleSubmit, validate } = useForm({
   initialValues: {
@@ -29,6 +45,34 @@ const { meta, handleSubmit, validate } = useForm({
 })
 
 let title = useField<string>('title')
+let tables = ref<Table[]>([{
+  floor: 1,
+  number: 1,
+  seatsNumber: 1
+}])
+
+/**
+ * если нет столиков, то добавляем столик на 1 этаже с 1 номером
+ * если есть столик, то добавляем новый столик на этаже предыдущего 
+ * столика с номером на 1 больше 
+ */
+function addTable() {
+  if (tables.value.length > 0) {
+    let prevTable = tables.value[tables.value.length - 1]
+    // добавляем новый столик на таком же этаже, что и предыдущий
+    tables.value.push({
+      floor: prevTable.floor,
+      number: Number(prevTable.number) + 1,
+      seatsNumber: prevTable.seatsNumber
+    })
+  } else {
+    tables.value.push({
+      floor: 1,
+      number: 1,
+      seatsNumber: 1
+    })
+  }
+}
 
 const submit = handleSubmit(async values => {
   loading.value = true
@@ -40,19 +84,62 @@ const submit = handleSubmit(async values => {
 })
 </script>
 <template>
-  <v-row class="justify-center">
-    <v-col cols="12" md="6" lg="4">
-      <v-form @submit.prevent="submit"
-        class="global-box d-flex flex-column align-center w-100 h-100 pt-4 pr-6 pb-4 pl-6">
-        <div class="font-weight-bold" style="font-size: 20px;">Создать</div>
+  <ClientOnly>
+    <v-row class="justify-center mb-8">
+      <v-col cols="12" md="8" lg="6">
+        <v-form @submit.prevent="submit"
+          class="global-box d-flex flex-column align-center w-100 h-100 pt-4 pr-6 pb-4 pl-6">
+          <div class="font-weight-bold" style="font-size: 20px;">Создать</div>
+          <h3 class="w-100">Главная информация</h3>
+          <v-text-field v-model="title.value.value" :error-messages="title.errorMessage.value"
+            placeholder="Название ресторана" variant="outlined" density="compact" class="mt-3 w-100" />
+          <h3 class="w-100 d-flex align-center">Столики<v-btn @click="addTable">добавить</v-btn></h3>
+          <div class="w-100">
 
-        <v-text-field v-model="title.value.value" :error-messages="title.errorMessage.value" placeholder="Заголовок"
-          variant="outlined" density="compact" class="mt-3 w-100" />
+          </div>
 
-        <v-btn class="ma-auto mt-4" variant="tonal" type="submit" :loading="loading" :disabled="!meta.valid">
-          Отправить
-        </v-btn>
-      </v-form>
-    </v-col>
-  </v-row>
+          <!-- <v-row v-for="table of tables" class="pa-0 d-flex justify-space-between">
+          <v-col class="pa-0" :cols="4">
+            {{ table.number }}
+          </v-col>
+          <v-col class="pa-0" :cols="4">
+            {{ table.seatsNumber }}
+          </v-col>
+          <v-col class="pa-0" :cols="4">
+            {{ table.floor }}
+          </v-col>
+        </v-row> -->
+          <v-data-table :items="tables" :headers="tablesHeaders" :items-per-page="5"no-data-text="Нет столиков" v-model:page="tablePage">
+            <template v-slot:item.number="{ item }">
+              <v-text-field variant="plain" type="number" v-model="item.number" :min="0"></v-text-field>
+            </template>
+
+            <template v-slot:item.floor="{ item }">
+              <v-text-field variant="plain" type="number" v-model="item.floor"></v-text-field>
+            </template>
+            
+            <template v-slot:item.seatsNumber="{ item, index }">
+              <div class="d-flex justify-space-between align-center">
+                <v-text-field variant="plain" type="number" v-model="item.seatsNumber" :min="1"></v-text-field>
+                <v-tooltip text="Удалить" location="top">
+                  <template v-slot:activator="{ props }">
+                    <v-btn v-bind="props" icon="mdi-delete-outline" variant="plain" color="red"
+                      @click="tables.splice(index, 1)">
+                    </v-btn>
+                  </template>
+                </v-tooltip>
+              </div>
+            </template>
+
+            <template v-slot:bottom>
+              <v-pagination v-model="tablePage" :length="Math.ceil(tables.length / 5)"></v-pagination>
+            </template>
+          </v-data-table>
+          <v-btn class="ma-auto mt-4" variant="tonal" type="submit" :loading="loading" :disabled="!meta.valid">
+            Отправить
+          </v-btn>
+        </v-form>
+      </v-col>
+    </v-row>
+  </ClientOnly>
 </template>
