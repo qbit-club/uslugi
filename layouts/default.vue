5a715ffa290fb49b60830591eb404e014d7453d4
JetGreen <grishadzyin@gmail.com>
2024-06-22 14:53:47 +0400

    come beauty

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup>
+const router = useRouter()
+
 let navigationDrawer = ref<boolean>(false)
+
+function navigateTo(route: any) {  
+  router.push(route.id)
+}
 </script>
 <template>
   <v-responsive>
@@ -8,22 +14,22 @@ let navigationDrawer = ref<boolean>(false)
       <v-app-bar :elevation="0">
         <v-row class="d-flex space-between">
           <v-col :cols="2">
-            <v-icon icon="$vuetify" class="ml-6"></v-icon>
+            <v-icon icon="$vuetify" class="ml-6" @click="router.push('/')"></v-icon>
           </v-col>
           <v-col :cols="8">main content</v-col>
           <v-col :cols="2" class="d-flex justify-end">
-            <v-icon icon="mdi-hamburger" class="mr-6" @click="navigationDrawer = !navigationDrawer"/>
+            <v-icon icon="mdi-hamburger" class="mr-6" @click="navigationDrawer = !navigationDrawer" />
           </v-col>
         </v-row>
       </v-app-bar>
 
-      <v-navigation-drawer :model-value="navigationDrawer" location="right" temporary>
-        <v-list nav>
-          <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
-          <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <ClientOnly>
+        <v-navigation-drawer :model-value="navigationDrawer" location="right" temporary>
+          <v-list nav @click:select="navigateTo">
+            <v-list-item prepend-icon="mdi-table-chair" title="Создать зал" value="/hall/create"></v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+      </ClientOnly>
 
       <v-main>
         <!-- index.vue goes here -->
