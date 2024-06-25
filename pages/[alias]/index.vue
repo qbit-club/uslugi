ecd24319547b4aa97b7e58d29566653ddd94d8fc
JetGreen <grishadzyin@gmail.com>
2024-06-25 18:36:20 +0400

    commit

--- pages/[alias]/index.vue
+++ pages/[alias]/index.vue
@@ -6,13 +6,13 @@ let route = useRoute()
 let alias = String(route.params.alias) ?? ''
 let res = await restStore.getByAlias(alias)
 
-console.log(res);
+let rest = res.data
 </script>
 <template>
   <v-container>
     <v-row>
       <v-col :cols="12">
-        <h2>{{ res.data }}</h2>
+        <h2>{{ rest }}</h2>
       </v-col>
     </v-row>
   </v-container>
