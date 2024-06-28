f1339b62e1babc94158f59d3391eddf601ca861a
JetGreen <grishadzyin@gmail.com>
2024-06-28 18:13:10 +0400

    display logo

--- pages/[alias]/index.vue
+++ pages/[alias]/index.vue
@@ -2,22 +2,24 @@
 const restStore = useRest()
 
 let route = useRoute()
-let rest = ref({})
+let rest = ref<any>({})
 
 let alias = String(route.params.alias) ?? ''
 
-  let res = await restStore.getByAlias(alias)
+let res = await restStore.getByAlias(alias)
 
-  rest = res.data
+rest.value = res.data.value
 
-
+let logo = ref(rest.value.images.filter((e: string) => e.includes('logo'))[0])
 </script>
 <template>
   <v-container>
     <v-row>
       <v-col :cols="12">
-        <h2>{{ rest }}</h2>
+        
+        <v-avatar :image="logo" size="250"></v-avatar>
       </v-col>
+      <h2>{{ rest }}</h2>
     </v-row>
   </v-container>
 </template>
