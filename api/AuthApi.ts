789feb1aaca5e4fcf233e4835516cf40984104d1
JetGreen <grishadzyin@gmail.com>
2024-07-02 18:10:12 +0400

    registration works

---
+++ api/AuthApi.ts
@@ -0,0 +1,23 @@
+export default {
+  registration(user: any): Promise<any> {
+    return useApiFetch('/auth/registration', { method: 'POST', body: user })
+  },
+  // login(email: string, password: string) {
+  // 	return $api.post('/auth/login', { email, password })
+  // },
+  // resetPassword(password: string, token: string) {
+  // 	return $api.post('/auth/reset-password', { password, token })
+  // },
+  // forgotPassword(email: string) {
+  // 	return $api.post('/auth/forgot-password', { email })
+  // },
+  // logout() {
+  // 	return $api.post('/auth/logout')
+  // },
+  // updateUser(user: any) {
+  // 	return $api.post('/auth/update', { user })
+  // },
+  // refresh() {
+  // 	return $api.get('/auth/refresh')
+  // },
+}
