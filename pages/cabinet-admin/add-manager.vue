4ea1423bdf711c84f373c425381f567a9180b049
regtyit <titov54275@gmail.com>
2024-07-16 19:39:54 +0400

    small change

--- pages/cabinet-admin/add-manager.vue
+++ pages/cabinet-admin/add-manager.vue
@@ -1,3 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
 <template>
-    добавляем менеджера
+    <v-row>
+        <v-col cols="4">
+
+            <v-select label="Рестораны" :items="['sus', 'amogus']">
+            </v-select>
+        </v-col>
+        <v-col cols="8">
+            <v-text-field>
+            </v-text-field>
+
+        </v-col>
+    </v-row>
+
 </template>
+
+<style scoped></style>
