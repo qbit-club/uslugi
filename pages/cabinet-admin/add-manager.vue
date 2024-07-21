8f19943d962944b87f470a1f174627e86f54bb7d
JetGreen <grishadzyin@gmail.com>
2024-07-21 14:13:03 +0400

    Revert "add manager form"
    This reverts commit b20fb7ce96212c63d4d7cba6e056bacaf389084c.

--- pages/cabinet-admin/add-manager.vue
+++ pages/cabinet-admin/add-manager.vue
@@ -1,25 +1,3 @@
-<script setup lang="ts">
-async function submit() {
-  
-}
-</script>
 <template>
-  <v-container>
-    <v-row>
-      <v-col :cols="12">
-        <h3>Добавить менеджера</h3>
-      </v-col>
-      <v-col :cols="6">
-        email
-        <v-text-field variant="outlined" density="compact"></v-text-field>
-      </v-col>
-      <v-col :cols="6">
-        alias ресторана
-        <v-text-field variant="outlined" density="compact"></v-text-field>
-      </v-col>
-      <v-col :cols="12" class="d-flex justify-center">
-        <v-btn variant="tonal" size="large" @click="submit">отправить</v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
+    добавляем менеджера
 </template>
