1ac30c220c6c34686ec840433e8414649b4c2e5f
qbclub <17926057+qbclub@users.noreply.github.com>
2024-07-22 18:31:53 +0400

    create mial form

--- pages/cabinet-manager/create-meal.vue
+++ pages/cabinet-manager/create-meal.vue
@@ -75,7 +75,7 @@ async function submit() {
 }
 </script>
 <template>
-  <v-row>
+  <v-row class="mb-16">
     <v-col cols="12">
       <h3>Создать блюдо</h3>
     </v-col>
@@ -83,43 +83,63 @@ async function submit() {
       <MenuItemImageInput @upload-menu-item-image="uploadImage" />
       <v-img v-for="pr of previews" :src="pr" class="img-preview"></v-img>
     </v-col>
-    <v-col cols="6">
+    <v-col cols="12" md="6">
       Название
-      <v-text-field v-model="form.name" density="compact" variant="outlined"></v-text-field>
+      <v-text-field hide-details v-model="form.name" density="compact" variant="outlined"></v-text-field>
     </v-col>
-    <v-col cols="6">
+    <v-col cols="12" md="6">
       Категория
-      <v-select v-model="form.category" :items="CATEGORIES" placeholder="пасты" variant="outlined"
-        density="compact"></v-select>
+      <v-select hide-details v-model="form.category" :items="CATEGORIES" placeholder="пасты" variant="outlined"
+        density="compact" clearable></v-select>
     </v-col>
-    <v-col cols="2">
+    <v-col cols="12" md="3" class="d-flex flex-column justify-end">
+
       Белки
-      <v-text-field type="number" v-model="form.health.protein" density="compact" variant="outlined" suffix="г."></v-text-field>
+      <div>
+        <v-text-field hide-details type="number" v-model="form.health.protein" density="compact" variant="outlined"
+          suffix="г."></v-text-field>
+      </div>
+
     </v-col>
-    <v-col cols="2">
+    <v-col cols="12" md="3" class="d-flex flex-column justify-end">
       + Жиры
-      <v-text-field type="number" v-model="form.health.fat" density="compact" variant="outlined" suffix="г."></v-text-field>
+      <div>
+        <v-text-field hide-details type="number" v-model="form.health.fat" density="compact" variant="outlined"
+          suffix="г."></v-text-field>
+      </div>
+
     </v-col>
-    <v-col cols="2">
+    <v-col cols="12" md="3" class="d-flex flex-column justify-end">
       + Углеводы
-      <v-text-field type="number" v-model="form.health.carb" density="compact" variant="outlined" suffix="г."></v-text-field>
+      <div>
+        <v-text-field hide-details type="number" v-model="form.health.carb" density="compact" variant="outlined"
+          suffix="г."></v-text-field>
+      </div>
+
     </v-col>
-    <v-col cols="2">
+    <v-col cols="12" md="3" class="d-flex flex-column justify-end">
       = Энергетическая ценность
-      <v-text-field v-model="form.health.energy" density="compact" variant="outlined"></v-text-field>
+      <div>
+        <v-text-field hide-details v-model="form.health.energy" density="compact" variant="outlined"></v-text-field>
+      </div>
+
     </v-col>
-    <v-col cols="4">
-      Масса
-      <v-text-field v-model="form.health.mass" density="compact" variant="outlined" placeholder="500 г"></v-text-field>
-    </v-col>
-    <v-col cols="9">
-      Состав
-      <v-text-field v-model="form.health.ingredients" density="compact" variant="outlined" placeholder="Из слабосолёного лосося, с тартаром из огурцов, красной икрой, укропом, луком шнитт и шалот"></v-text-field>
-    </v-col>
-    <v-col cols="3">
+    <v-col cols="12" md="6">
       Цена
-      <v-text-field v-model="form.price" prefix="₽" density="compact" variant="outlined"></v-text-field>
+      <v-text-field hide-details v-model="form.price" prefix="₽" density="compact" variant="outlined"></v-text-field>
     </v-col>
+    <v-col cols="12" md="6">
+      Масса
+      <v-text-field hide-details v-model="form.health.mass" density="compact" variant="outlined"
+        placeholder="500 г"></v-text-field>
+    </v-col>
+    <v-col cols="12">
+      Состав
+      <v-textarea auto-grow hide-details :rows="1" v-model="form.health.ingredients" density="compact"
+        variant="outlined"
+        placeholder="Из слабосолёного лосося, с тартаром из огурцов, красной икрой, укропом, луком шнитт и шалот"></v-textarea>
+    </v-col>
+
     <v-col cols="12" class="d-flex justify-center">
       <v-btn size="large" variant="tonal" :loading="loading" @click="submit">отправить</v-btn>
     </v-col>
