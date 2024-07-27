b578429613c98a7ea346d21e75cd564a34f0eb6b
regtyit <titov54275@gmail.com>
2024-07-27 14:17:14 +0400

    comm

--- pages/cabinet-admin/add-manager.vue
+++ pages/cabinet-admin/add-manager.vue
@@ -2,29 +2,34 @@
 import type { Rest } from '~/types/rest.interface';
 import type { User } from '~/types/user.interface';
 
-const auth = useAuth()
-
 const restStore = useRest()
 const authStore = useAuth()
 let { data } = await restStore.get()
-let rest_ids = ref<[]>()
-let rests_with_managers = ref<[]>()
+let rest_ids = ref()
+let rests_with_managers = ref<any[]>()
 let chosen_rest = ref<any>("")
-let user_email = ref<string>("")
+let user_email = ref("")
 
 async function setManager() {
   await authStore.setManager(user_email.value, chosen_rest.value)
-  // let rest_with_managers = restStore.getManagersOfRest(rest.alias).map((user) => user.email)
-  // let index = rests_with_managers.value.findIndex((rest) => rest.alias == chosen_rest.value);
-  // rests_with_managers[index]=rest_with_managers
+
+  let managers: any[] = await restStore.getManagersOfRest(chosen_rest.value)
+  managers = managers.map((user: User) => user.email)
+
+  let index: Number = rests_with_managers.value!.findIndex((rest: any) => rest.alias == chosen_rest.value);
+  rests_with_managers.value ? [index] = managers:[]
 }
 
 onMounted(async () => {
-  // for (rest in data) {
-  //   rest_ids.value.push({ name: rest.title, alias: rest.alias })
-  //   // let rest_with_managers = restStore.getManagersOfRest(rest.alias).map((user) => user.email)
-  //   // rests_with_managers.value.push({ name: rest.title, managers: managers })
-  // }
+  for (let rest of data.value) {
+    rest_ids?.value?.push({ name: rest.title, alias: rest.alias })
+
+    let managers = await restStore.getManagersOfRest(rest.alias)
+    // console.log(managers.data)
+    // managers.data = managers.data.map((user: User) => user.email)
+
+    rests_with_managers?.value?.push({ name: rest.title, managers: managers })
+  }
 })
 </script>
 
@@ -45,11 +50,11 @@ onMounted(async () => {
   <v-btn variant="tonal" @click="setManager()">Cделать менеджером</v-btn>
 
   <h3>Управление менеджерами</h3>
-  <v-row>
+  <!-- <v-row>
     <v-col cols="12" md="3" v-for="rest in data">
 
     </v-col>
-  </v-row>
+  </v-row> -->
 </template>
 
 <style scoped></style>
