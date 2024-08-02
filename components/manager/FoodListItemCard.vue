45af2cf3ef1c4d3efd0acc97d61a4fa2b9d5aa37
qbclub <17926057+qbclub@users.noreply.github.com>
2024-08-02 13:11:58 +0400

    manage menu

--- components/manager/FoodListItemCard.vue
+++ components/manager/FoodListItemCard.vue
@@ -1,60 +1,78 @@
 <script setup lang="ts">
 import type { FoodListItemFromDb } from '@/types/food-list-item-from-db.interface'
 
-defineProps<{
+const props = defineProps<{
   item: FoodListItemFromDb
+  inMenu: boolean | undefined
 }>()
-const emit = defineEmits(['moveToMenu'])
+const emit = defineEmits(['moveToMenu', 'deleteFromMenu'])
 
-function getHealth(h: {
-  protein: Number,
-  fat: Number,
-  carb: Number,
-  energy: String
-}) {
-  return `белки ${h.protein}г. жиры ${h.fat}г. углеводы ${h.carb}г. / ${h.energy}`
+let isMenuItem = ref(false)
+
+function toggleMenu() {
+  isMenuItem.value ? emit('moveToMenu', props.item._id) : emit('deleteFromMenu', props.item._id)
 }
+
+watch(isMenuItem, () => {
+  toggleMenu()
+})
+onMounted(() => {
+
+  isMenuItem.value = props.inMenu
+  // toggleMenu()
+})
+
 </script>
 <template>
-  <v-card class="h-100">
-    <v-img :src="item?.images[0]" cover></v-img>
-    <v-card-title>
-      {{ item.name }}
-    </v-card-title>
 
-    <v-card-text>
-      {{ item.health.ingredients }}
-      <div class="health">
-        {{ getHealth(item.health) }}
+  <v-row class="d-flex align-center justify-space-between" :class="[isMenuItem ? 'in-menu' : 'out-menu']">
+    <v-col class="d-flex align-center  cursor-pointer">
+
+      <img :src="item?.images[0]" style="height: 50px;" aspect-ratio="1"></img>
+      <div class="pa-4">
+        {{ item.category }}
+      <h4> {{ item.name }}</h4> 
       </div>
-    </v-card-text>
-    <div class="actions-container">
-      <v-btn variant="flat" rounded type="primary" size="small" @click="emit('moveToMenu', item._id)">
-        <v-icon icon="mdi-chevron-up" style="font-size: 24px;"></v-icon>
-      </v-btn>
-    </div>
-    <div class="price-container">
-      <v-btn variant="flat" rounded type="primary" size="small" :ripple="false">
-        {{ item.price }} ₽
-      </v-btn>
-    </div>
-  </v-card>
+      <div>
+      <h4 class="pa-4"> {{ item.price }} ₽</h4> 
+      </div>
+
+
+    </v-col>
+    <v-col cols="12" md="4" class="pa-0">
+      <div class="d-flex justify-center" :class="[isMenuItem ? 'in-menu' : 'out-menu']">
+
+        <div class="d-flex align-center justify-center cursor-pointer">
+
+          <div class="d-flex flex-column align-center pa-2 " @click="">
+            <v-icon icon="mdi-pencil" size="x-large" />
+            <div class="explanation text-center">редактировать</div>
+
+          </div>
+
+          <div class="d-flex flex-column align-center pa-2" @click="">
+            <v-icon icon="mdi-trash-can-outline" size="x-large" />
+            <div class="explanation text-center">удалить</div>
+
+          </div>
+          <div class="d-flex  align-center pa-2">
+            <v-checkbox v-model="isMenuItem" hide-details class="pa-0"></v-checkbox>
+            <div class="text-center text-uppercase" v-if="isMenuItem">в меню</div>
+
+          </div>
+        </div>
+      </div>
+    </v-col>
+  </v-row>
+  <v-divider></v-divider>
+
 </template>
 <style scoped lang="scss">
-.actions-container {
-  top: 4px;
-  right: 4px;
-  position: absolute;
+.in-menu {
+  color: green;
 }
 
-.price-container {
-  top: 4px;
-  left: 4px;
-  position: absolute;
-}
-
-.health {
-  font-size: 10px;
-  font-weight: 250;
+.out-menu {
+  opacity: 0.7;
 }
 </style>
