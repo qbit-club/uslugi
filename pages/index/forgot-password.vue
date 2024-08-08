b537a5bd1fcb868da870b687334a314c4422aab7
JetGreen <grishadzyin@gmail.com>
2024-08-08 17:00:56 +0400

    add feedback

--- pages/index/forgot-password.vue
+++ pages/index/forgot-password.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { toast } from 'vue3-toastify';
+
 const authStore = useAuth()
 
 const router = useRouter()
@@ -15,22 +17,28 @@ async function sendResetLink() {
 let newPassword = ref<string>('')
 async function resetPassword() {
   let res = await authStore.resetPassword(newPassword.value, String(route.query.user_id), String(route.query.token))
+  if (res.status.value == "success") {
+    toast('Пароль изменён!', { type: "success" })
+    await authStore.checkAuth()
+    router.push('/')
+  }
 }
 </script>
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" sm="10" md="7" lg="5" class="ma-auto" >
+      <v-col cols="12" sm="10" md="7" lg="5" class="ma-auto">
         <div class="text-h6 font-weight-bold pa-8 text-center">Восстановление пароля</div>
 
-        <v-form @submit.prevent="sendResetLink" class="d-flex flex-column align-center justify-center w-100" v-if="!route.query.user_id && !route.query.token">
+        <v-form @submit.prevent="sendResetLink" class="d-flex flex-column align-center justify-center w-100"
+          v-if="!route.query.user_id && !route.query.token">
           <v-text-field label="Email" type="email" variant="outlined" density="compact" class="w-100"
             v-model="forgotEmail"></v-text-field>
           <v-btn type="submit" color="primary" html-type="submit">отправить</v-btn>
         </v-form>
         <v-form v-else class="d-flex flex-column align-center justify-center w-100" @submit.prevent="resetPassword">
           <v-text-field label="Новый пароль" type="password" variant="outlined" density="compact" class="w-100"
-          v-model="newPassword"></v-text-field>
+            v-model="newPassword"></v-text-field>
           <v-btn type="submit" color="primary" html-type="submit">отправить</v-btn>
         </v-form>
 
